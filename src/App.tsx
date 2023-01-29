859f96142c0b8256b1eb84322e94892cb0872d13
Salman Mohamed <salman.mohamed@expereo.com>
2023-01-29 10:56:00 +0100

    [FEAT] Added responsiveness to the app page

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ const App: FC = () => {
   useDocumentTitle("Hiragana game | Select your game");
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen space-y-4">
+    <div className="flex flex-col items-center justify-center h-screen w-screen space-y-4 p-4">
       <h1>Select your game. / げむおせんたくしま。</h1>
-      <div className="flex space-x-4">
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
         <Link className="bg-gray-700 shadow-xl p-4 rounded" to="/memory">
           めもり / Memory 🧠
         </Link>
